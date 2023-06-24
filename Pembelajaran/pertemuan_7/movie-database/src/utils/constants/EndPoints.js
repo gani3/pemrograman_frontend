29fa89c1fd3547d60ea15e62379b77068b94178d
gani3 <ganialadul@gmail.com>
2023-06-24 14:44:07 +0700

    update endpoint

--- Pembelajaran/pertemuan_7/movie-database/src/utils/constants/EndPoints.js
+++ Pembelajaran/pertemuan_7/movie-database/src/utils/constants/EndPoints.js
@@ -2,9 +2,9 @@ const API_KEY = process.env.REACT_APP_API_KEY
 const BASE_URL = 'https://api.themoviedb.org/3/movie/'
 
 const EndPoint = {
-    POPULAR : `${BASE_URL}popular/?api_key=${API_KEY}`,
-    NOWPLAYING : `${BASE_URL}now_playing/?api_key=${API_KEY}`,
-    TOPRATED : `${BASE_URL}top_rated/?api_key=${API_KEY}`,
+    POPULAR : `${BASE_URL}popular?api_key=${API_KEY}`,
+    NOWPLAYING : `${BASE_URL}now_playing?api_key=${API_KEY}`,
+    TOPRATED : `${BASE_URL}top_rated?api_key=${API_KEY}`,
     DetailMovie(id){
         return `${BASE_URL}${id}?api_key=${API_KEY}&append_to_response=videos`;
     },
